254290a83e6f3f1629a740ecc806a98d34ce755d
Lucas Lima <lucaserolima@gmail.com>
2024-03-26 21:56:22 -0400

    First Commit - Microservices Project

---
+++ service-registry/src/test/java/br/com/ero/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
